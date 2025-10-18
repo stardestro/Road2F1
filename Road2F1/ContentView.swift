7233536b96e0a2642339ccbedf89062193000883
stardestro <25290886+stardestro@users.noreply.github.com>
2025-10-18 16:49:08 -0500

    Initial Commit

---
+++ Road2F1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Road2F1
+//
+//  Created by Hussain Alkatheri on 10/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Road2F1/Road2F1App.swift
@@ -0,0 +1,17 @@
+//
+//  Road2F1App.swift
+//  Road2F1
+//
+//  Created by Hussain Alkatheri on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Road2F1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
